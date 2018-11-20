8c641284823aa519ee2d2bd0818c553975e15990
Bryson Spaulding <brysonian10@hotmail.com>
2018-11-20 08:08:01 -0700

    Created the project and added a model Duck.

---
+++ src/gui/controller/GUIController.java
@@ -0,0 +1,17 @@
+package gui.controller;
+import gui.model.Duck;
+
+public class GUIController
+{
+	private Duck myDuck;
+	
+	GUIController()
+	{
+		myDuck= new Duck();
+	}
+	
+	public void start()
+	{
+		
+	}
+}
